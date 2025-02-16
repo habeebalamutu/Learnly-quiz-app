d950b407a21b0ab83ee0d95ca6f1969e999817e6
Habeeb Alamutu <habeebalamutu04@gmail.com>
2025-02-16 13:07:04 -0800

    p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,49 @@
-
 import React from "react";
+  
 import { Routes, Route } from "react-router-dom";
+  
 import Home from "./pages/Home";
+  
 import Quiz from "./pages/Quiz";
+  
 import DragDropQuiz from "./pages/DragDropQuiz";
+  
 
+  
 
+  
 import { DndProvider } from "react-dnd";
+  
 import { TouchBackend } from "react-dnd-touch-backend";
+  
 
+  
 function App() {
+  
   return (
+  
 
+  
     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+  
       <Routes>
+  
         <Route path="/" element={<Home />} />
+  
         <Route path="/objective-quiz" element={<Quiz />} />
+  
         <Route path="/drag-drop-quiz" element={<DragDropQuiz />} />
+  
       </Routes>
+  
     </DndProvider>
+  
   );
+  
 }
+  
 
+  
 export default App;
-
+  
+  
