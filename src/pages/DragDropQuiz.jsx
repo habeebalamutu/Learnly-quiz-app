fcafa5d52da638175c15a34ba880db2f484317d1
Habeeb Alamutu <habeebalamutu04@gmail.com>
2025-02-15 18:32:12 -0800

    Fix path for DragDropQuiz.css

--- src/pages/DragDropQuiz.jsx
+++ src/pages/DragDropQuiz.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import "../styles/DragDropQuiz.css";
+import "../styles/dragdropquiz.css";
 
 
 const initialDragOptions = [
